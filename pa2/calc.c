6a841eda4f30d2fa51f512b85563cf93b74ee037
Joeb3219 <joseph.a.boyle@rutgers.edu>
2016-10-04 00:09:07 -0400

    More structure completed. What now, :(

--- pa2/calc.c
+++ pa2/calc.c
@@ -8,14 +8,8 @@ typedef unsigned int uint;
 
 typedef enum base{DEC = 10, BIN = 2, HEX = 16, OCT = 7} Base;
 
-typedef struct byte{
-	uint decVal;
-	//char representation[8];
-} Byte;
-
 typedef struct number{
-	char *representation;
-	Byte **bytes;
+	int *representation, digits;
 	Base base;
 	int negative;
 } Number;
@@ -53,51 +47,39 @@ void convertBase(Number *number, Base toBase){
 
 int charToDig(char c){
 	if(c >= '0' && c <= '9') return c - '0';
-	if(c >= 'a' && c <= 'f') return c - 'a';
-	else return c - 'A';
+	if(c >= 'a' && c <= 'f') return c - 'a' + 10;
+	else return c - 'A' + 10;
 }
 
-Byte** convertStringToBytes(char *string, Base base){
-	int i, currByte, numBytes = (strlen(string) + 7) / 8;
-	Byte **bytes = malloc(numBytes * sizeof(Byte));
-	for(i = 0; i < strlen(string); i ++){
-		printf("%d, currByte: %d\n", i, currByte);
-		currByte = (i + 8) / 8;
-		if(bytes[currByte] == 0){
-			bytes[currByte] = malloc(sizeof(Byte));
-			bytes[currByte]->decVal = 0;
-		}
-		printf("Value of decVal: %d, value of added: %d\n", bytes[currByte]->decVal, (base * (i - (currByte * 8)) * string[i]));
-		bytes[currByte]->decVal += (base * (i - (currByte * 8)) * charToDig(string[i]));
-	}
-	return bytes;
-}
-
-void strrev(char *string){
+char* strrev(char *string){
 	int i = 0, j = strlen(string) - 1;
-	char temp;
-	for(i = 0; i < j / 2; i ++){
-		temp = string[i];
-		string[i] = string[j - i];
-		string[j - i] = temp;
+	char *final = malloc(strlen(string) + 1);
+	for(i = 0; i < j + 1; i ++){
+		final[i] = string[j - i];
 	}
+	final[strlen(string)] = '\0';
+	return final;
 }
 
 Number* formNumber(char *representation){
 	char *realRepresentation;
+	int i;
 	if(strlen(representation) == 0) return NULL;
 	Number *number = malloc(sizeof(Number));
 	if(representation[0] == '-'){
 		number->negative = 1;
-		 realRepresentation = representation + 1; // Skip the sign, we will refer back to it later.
+		realRepresentation = representation + 1; // Skip the sign, we will refer back to it later.
 	}else{
 		number->negative = 0;
-		 realRepresentation = representation;
+		realRepresentation = representation;
 	}
 	number->base = getBaseByChar(realRepresentation[0]);
-	strrev(realRepresentation + 1);
-	number->bytes = convertStringToBytes(realRepresentation + 1, number->base);
-	number->representation = realRepresentation + 1;
+	realRepresentation += 1;
+	number->digits = strlen(realRepresentation);
+	number->representation = malloc(number->digits * sizeof(int));
+	for(i = 0; i < number->digits; i ++){
+		number->representation[i] = charToDig(realRepresentation[i]);
+	}
 	return number;
 }
 
@@ -105,8 +87,18 @@ void freeNumber(Number *number){
 	free(number);
 }
 
+char digToChar(int i){
+	if(i < 10) return '0' + i;
+	return 'A' + (i - 10);
+}
+
 void printNumber(Number *number){
-	printf("[Num]: %c%s(%d)\n", (number->negative == 1) ? '-' : ' ',number->representation, number->base);
+	int i;
+	printf("[Num]: %c(%d)", (number->negative == 1) ? '-' : ' ', number->base);
+	for(i = 0; i < number->digits; i ++){
+		printf("%c", digToChar(number->representation[i]));
+	}
+	printf("\n");
 }
 
 int main(int argv, char **argc){
@@ -116,18 +108,25 @@ int main(int argv, char **argc){
 	}
 
 	char *opsign, *format;
-	Number *number1, *number2;	
+	Number *number1, *number2, *result;	
 	
 	opsign = argc[1];
 	format = argc[4];
 	number1 = formNumber(argc[2]);
 	number2 = formNumber(argc[3]);
+	if(opsign == '+') result = add(number1, number2, PRESERVE);
+	else result = subtract(number1, number2, PRESERVE);
 
 	printNumber(number1);
 	printNumber(number2);
+	printf("The result (not based): ");
+	printNumber(result);
+	printf("The result (based): ");
+	convertBase(result, getBaseByChar(format)); 
+	printNumber(result);
 
 	freeNumber(number1);
 	freeNumber(number2);
-	
+		
 	return 0;
 }
